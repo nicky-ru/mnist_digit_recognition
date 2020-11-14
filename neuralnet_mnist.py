827c061a5e99743ed4c0493dd94d1060b588a0ba
nicky_ru <nikitkaruban@gmail.com>
2020-11-14 12:40:18 +0800

    add test and train accuracy statistic for each epoch

--- neuralnet_mnist.py
+++ neuralnet_mnist.py
@@ -8,15 +8,18 @@ import pickle
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
 
 train_lost_list = []
+train_acc_list = []
+test_acc_list = []
 
 iters_num = 100
 train_size = x_train.shape[0]
 batch_size = 100
-learning_rate = 0.1
+learning_rate = 1
+iter_per_epoch = max(train_size / batch_size, 1)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-for _ in range(iters_num):
+for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
@@ -28,7 +31,12 @@ for _ in range(iters_num):
 
     loss = network.loss(x_batch, t_batch)
     train_lost_list.append(loss)
-    print(loss)
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print('train acc, test acc | ' + str(train_acc) + ', ' + str(test_acc))
 
 
 x = np.arange(len(train_lost_list))
