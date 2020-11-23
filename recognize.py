f79905f4f9459090be7672cc87bc3f3349c0a688
nicky_ru <nikitkaruban@gmail.com>
2020-11-23 12:35:22 +0800

    4 layer NN 784-100-100-50-10 softmax with loss, 10000 iters, batch_size = 100, lr = 0.1, train acc, test acc | 0.9858666666666667, 0.9677

--- recognize.py
+++ recognize.py
@@ -7,7 +7,7 @@ img = get_img('dataset/WechatIMG205.jpeg')
 
 network = FourLayerNet(input_size=784, first=100, second=100, third=50, output_size=10)
 
-with open('resources/leaned_weights.pkl', 'rb') as f:
+with open('resources/weights.pkl', 'rb') as f:
     network_to_unpickle = pickle.load(f)
     network.params = network_to_unpickle.get('params')
     network.layers = network_to_unpickle.get('layers')
